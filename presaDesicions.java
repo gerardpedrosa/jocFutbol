9165df66ad6c52efbd455f64c081ea83f42322cc
Gerard Pedrosa <alu.gerard.pedrosa@mataro.epiaedu.cat>
2025-10-03 21:51:10 +0200

    Benvinguda i decisió 1

---
+++ presaDesicions.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class presaDesicions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Benvinguda i instruccions
+        System.out.println("BENVINGUT A LA FINAL DEL TORNEIG");
+        System.out.println("Ets l'entrenador de l'equip. Les teves decisions poden fer que guanyis o perdis el partit.");
+        System.out.println();
+        System.out.println("Com funciona:");
+        System.out.println("Se't presentaran diverses decisions.");
+        System.out.println("Has d'escriure el NUMERO de l'opció que vulguis i prémer ENTER.");
+        System.out.println();
+
+        //Decisió
+        System.out.println("Decisió 0: Amb qui comences el partit?");
+        System.out.println("1.Amb els de sempre");
+        System.out.println("2.Amb la jove promesa");
+        int dec0 = scanner.nextInt();
+        boolean jove = (dec0 == 2); //això fa que si la opció que elegeixes es la dos sera true
+    }
+}
+
+
